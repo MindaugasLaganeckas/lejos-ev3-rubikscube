a50b76b33be5abd9d92dac1d45baa175533d2dd0
Mindaugas Laganeckas <mindaugas.laganeckas@gmail.com>
2024-12-24 17:21:55 +0100

    Migrate to Maven project

---
+++ ev3.rubikscube.client/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ev3.rubikscube.ui {
+    opens ev3.rubikscube.ui to javafx.graphics, javafx.fxml, javafx.controls, javafx.swing;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires opencv;
+}
